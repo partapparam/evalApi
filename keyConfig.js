7aa2a7a71f85adef414604f907d7cad44071e87f
partapparam <paramsingh1961@gmail.com>
2023-06-21 13:06:45 -0700

    env changes

--- keyConfig.js
+++ keyConfig.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const secretKey = fs.readFileSync("./jwtRS256.key")
-const publicKey = fs.readFileSync("./jwtRS256.key.pub")
+const secretKey = process.env.JWT_SECRET_KEY
+const publicKey = process.env.JWT_PUBLIC_KEY
 
 module.exports = { secretKey, publicKey }
