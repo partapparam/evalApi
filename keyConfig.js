2d104324d14630d714eaeb9ab6e7f410f8b1e561
partapparam <paramsingh1961@gmail.com>
2023-07-08 19:13:35 -0700

    config

--- keyConfig.js
+++ keyConfig.js
@@ -3,13 +3,13 @@ const fs = require("fs")
 /**
  * Use is Dev mode
  */
-const secretKey = fs.readFileSync("./jwtRS256.key")
-const publicKey = fs.readFileSync("./jwtRS256.key.pub")
+// const secretKey = fs.readFileSync("./jwtRS256.key")
+// const publicKey = fs.readFileSync("./jwtRS256.key.pub")
 
 /**
  * Use is Production mode
  */
-// const secretKey = process.env.JWT_SECRET_KEY
-// const publicKey = process.env.JWT_PUBLIC_KEY
+const secretKey = process.env.JWT_SECRET_KEY
+const publicKey = process.env.JWT_PUBLIC_KEY
 
 module.exports = { secretKey, publicKey }
