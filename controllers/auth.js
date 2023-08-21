5b2994f391340f37284aa77b058e02af74705901
partapparam <paramsingh1961@gmail.com>
2023-08-20 21:26:08 -0700

    confimr token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,10 @@
 const jwt = require("jsonwebtoken")
 const { secretKey } = require("../keyConfig")
-const nodemailer = require('nodemailer')
-const crypto = require('crypto')
+const nodemailer = require("nodemailer")
+const crypto = require("crypto")
 const authRouter = require("express").Router()
 const bcrypt = require("bcrypt")
 const db = require("../db/pg")
-
 
 /**
  * Queries
@@ -106,56 +105,85 @@ authRouter.post("/login", async (req, res) => {
  * Request Token
  */
 
-authRouter.post('/forgotPassword', (req, res) => {
-    if (req.body.email === '') {
-      res.status(400).send('email required');
-    }
+authRouter.post("/forgotPassword", async (req, res) => {
+  if (req.body.email === "") {
+    res.status(400).send("email required")
+  }
   console.error(req.body.email)
   // Get User from DB to ensure email exists
   const response = await validate(req.body.email)
-    const user = response.rows[0] 
-    
-      if (user === null) {
-        console.error('email not in database');
-        res.status(403).send('email not in db');
+  const user = response.rows[0]
+
+  if (user === null) {
+    console.error("email not in database")
+    res.status(403).send("email not in db")
+  } else {
+    const token = crypto.randomBytes(20).toString("hex")
+    const expires = Date.now() + 3600000
+    user.update({
+      resetPasswordToken: token,
+      resetPasswordExpires: Date.now() + 3600000,
+    })
+    await db.query(updateResetPasswordTokenQuery, [
+      token,
+      expires,
+      user.user_id,
+    ])
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: `${process.env.EMAIL_ADDRESS}`,
+        pass: `${process.env.EMAIL_PASSWORD}`,
+      },
+    })
+
+    const mailOptions = {
+      from: "mySqlDemoEmail@gmail.com",
+      to: `${user.email}`,
+      subject: "Link To Reset Password",
+      text:
+        "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+        "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
+        `http://localhost:3031/reset/${token}\n\n` +
+        "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+    }
+
+    console.log("sending mail")
+
+    transporter.sendMail(mailOptions, (err, response) => {
+      if (err) {
+        console.error("there was an error: ", err)
       } else {
-        const token = crypto.randomBytes(20).toString('hex');
-        user.update({
-          resetPasswordToken: token,
-          resetPasswordExpires: Date.now() + 3600000,
-        });
-
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: `${process.env.EMAIL_ADDRESS}`,
-            pass: `${process.env.EMAIL_PASSWORD}`,
-          },
-        });
-
-        const mailOptions = {
-          from: 'mySqlDemoEmail@gmail.com',
-          to: `${user.email}`,
-          subject: 'Link To Reset Password',
-          text:
-            'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
-            + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-            + `http://localhost:3031/reset/${token}\n\n`
-            + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
-        };
-
-        console.log('sending mail');
-
-        transporter.sendMail(mailOptions, (err, response) => {
-          if (err) {
-            console.error('there was an error: ', err);
-          } else {
-            res.status(200).json('recovery email sent');
-          }
-        });
+        res.status(200).json("recovery email sent")
       }
-    });
-  });
-};
+    })
+  }
+})
+
+/**
+ * Get Reset Token to validate request client side
+ */
+authRouter.get("/reset", async (req, res) => {
+  const response = await db.query(getPasswordResetTokenQuery, [req.body.token])
+  const user = response.rows[0]
+  // User.findOne({
+  //   where: {
+  //     resetPasswordToken: req.query.resetPasswordToken,
+  //     resetPasswordExpires: {
+  //       [Op.gt]: Date.now(),
+  //     },
+  //   },
+
+  if (user == null) {
+    console.error("password reset link is invalid or has expired")
+    res.status(403).send("password reset link is invalid or has expired")
+  } else {
+    res.status(200).send({
+      user: user.email,
+      message: "password reset link a-ok",
+    })
+  }
+})
 
 module.exports = authRouter
