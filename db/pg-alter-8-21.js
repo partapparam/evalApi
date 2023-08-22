6c0a807197c9d070a773235bdfd5bc5045824f84
partapparam <paramsingh1961@gmail.com>
2023-08-21 17:32:50 -0700

    sql alter

---
+++ db/pg-alter-8-21.js
@@ -0,0 +1,28 @@
+const { Client } = require("pg")
+const seed = require("./seed")
+require("dotenv").config()
+const client = new Client()
+
+const createScript = `
+ALTER TABLE users 
+ADD reset_password_token VARCHAR(30) NULL,
+ADD reset_password_expires INT NULL
+;
+`
+
+client.connect(async (err) => {
+  if (err) {
+    return console.error("could not connect to postgres", err)
+  }
+  try {
+    console.log("creating scripts")
+
+    await client.query(createScript)
+  } catch (err) {
+    console.log("recieved error---", err)
+    process.exit(1)
+  } finally {
+    //   release client
+    client.end()
+  }
+})
