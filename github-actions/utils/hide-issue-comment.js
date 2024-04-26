6b218d54e675a72737b9d3af6221f74801744afa
partapparam <paramsingh1961@gmail.com>
2024-04-25 18:34:57 -0700

    added rule to hide comment

---
+++ github-actions/utils/hide-issue-comment.js
@@ -0,0 +1,30 @@
+/**
+ * Posts a comment on github
+ * @param {Number} issueNum - the issue number where the comment should be posted
+ * @param {String} comment - the comment to be posted
+ */
+async function postComment(issueNum, comment, github, context) {
+  try {
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: issueNum,
+      body: comment,
+    })
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+async function HideComment(github, context, nodeID, reason) {
+    const resp = await github.graphql(`
+      mutation {
+        minimizeComment(input: {classifier: ${reason}, subjectId: "${nodeID}"}) {
+          minimizedComment {
+            isMinimized
+          }
+        }
+      }
+    `)
+
+module.exports = HideComment
