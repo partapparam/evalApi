cd8774aaa718d5922d325e523ebd7d5c90545926
partapparam <paramsingh1961@gmail.com>
2024-04-26 21:33:10 -0700

    comment

--- github-actions/issue-trigger/update-comments/add-comments.js
+++ github-actions/issue-trigger/update-comments/add-comments.js
@@ -8,7 +8,6 @@ var context
 async function main({ g, c }) {
   github = g
   context = c
-  console.log("context", context)
   const owner = context.repo.owner
   const repo = context.repo.repo
   const issueNumber = context.payload.issue.number
@@ -18,38 +17,5 @@ async function main({ g, c }) {
             Based on the \`feature: feature branch\` label, this issue should target a feature branch.  Please consult the instructions on [working off of a feature branch](https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch)`
   await postComment(issueNumber, commentBody, github, context)
 }
-
-// async function fetchComments({ github, context }) {
-//   let issueNumber = github.event.issue.number
-//   let owner = context.repo.owner
-//   let repo = context.repo.repo
-//   const comments = await github.rest.issues.listComments({
-//     owner: owner,
-//     repo: repo,
-//     issue_number: issueNumber,
-//   })
-//   return comments
-// }
-
-// export function findCommentPredicate(comment) {
-//   return comment.body.includes("string to match")
-// }
-
-// export function findMatchingComment(comments) {
-//   const matchingComments = comments.filter((comment) =>
-//     findCommentPredicate(comment)
-//   )
-//   const comment = matchingComments[0]
-//   console.log("matching comments", matchingComments)
-//   if (comment) {
-//     return comment
-//   }
-//   return undefined
-// }
-
-// export async function findComment() {
-//   const comments = await fetchComments()
-//   return findMatchingComment(comments)
-// }
 
 module.exports = main
