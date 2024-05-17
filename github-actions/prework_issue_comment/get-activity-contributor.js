3b3e34db99f6bb0dafd7eae9d69ff06b592dea13
partapparam <paramsingh1961@gmail.com>
2024-05-17 16:59:57 -0700

    fix

--- github-actions/prework_issue_comment/get-activity-contributor.js
+++ github-actions/prework_issue_comment/get-activity-contributor.js
@@ -15,17 +15,17 @@ async function main({ g, c }) {
   context = c
   console.log(context)
   //   console.log(context.eventName, context)
-  if (context.eventName == "issue_comment") {
-    return getIssueCommentEventType()
-  } else if (context.eventName == "issue") {
-    return getIssueEventType()
-  } else if (context.eventName == "pull_request") {
-    return getPullRequestEventType()
-  } else if (context.eventName == "pull_request_review") {
-    return getPullRequestReviewEventType()
-  } else if (context.eventName == "pull_request_review_comment") {
-    return getPullRequestReviewCommentEventType()
-  }
+  //   if (context.eventName == "issue_comment") {
+  //     return getIssueCommentEventType()
+  //   } else if (context.eventName == "issue") {
+  //     return getIssueEventType()
+  //   } else if (context.eventName == "pull_request") {
+  //     return getPullRequestEventType()
+  //   } else if (context.eventName == "pull_request_review") {
+  //     return getPullRequestReviewEventType()
+  //   } else if (context.eventName == "pull_request_review_comment") {
+  //     return getPullRequestReviewCommentEventType()
+  //   }
   //
 }
 
