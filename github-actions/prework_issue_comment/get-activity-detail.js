25c5c20b74e7c9c51c3b0d61e8e8d5bbbe3a4d42
partapparam <paramsingh1961@gmail.com>
2024-05-20 23:30:37 -0700

    documentation

--- github-actions/prework_issue_comment/get-activity-detail.js
+++ github-actions/prework_issue_comment/get-activity-detail.js
@@ -6,29 +6,41 @@ var github
 var context
 
 /**
- * @description - This function is the entry point into the javascript file, it formats the md file and posts the comment on the issue
+ * @description - This function is the entry point into the javascript file,
+ * It will determine the eventName and return the proper activity detail
  * @param {Object} g - github object
  * @param {Object} c - context object
  */
 async function main({ g, c }) {
   github = g
   context = c
-  console.log(context)
-  // console.log(context.eventName)
-  if (context.eventName == "issue_comment") {
-    return await getIssueCommentEventType(context)
-  } else if (context.eventName == "issues") {
-    return await getIssueEventType(context)
-  } else if (context.eventName == "pull_request") {
-    return await getPullRequestEventType(context)
-  } else if (context.eventName == "pull_request_review") {
-    return await getPullRequestReviewEventType(context)
-  } else if (context.eventName == "pull_request_review_comment") {
-    return await getPullRequestReviewCommentEventType(context)
+
+  switch (context.eventName) {
+    case "issue_comment":
+      return await getIssueCommentEventType(context)
+    case "issues":
+      return await getIssueEventType(context)
+    case "pull_request":
+      return await getPullRequestEventType(context)
+    case "pull_request_review":
+      return await getPullRequestReviewEventType(context)
+    case "pull_request_review_comment":
+      return await getPullRequestReviewCommentEventType(context)
+    default:
+      // Handle cases where eventName doesn't match any known event type
+      console.log(`Unknown event name: ${context.eventName}`)
   }
-  //
 }
 
+/**
+ * Retrieves detailed information about an issue event based on the action performed.
+ * @param {*} context
+ * @returns - An object containing the activity detail:
+ * - contributor
+ * - action
+ * - object the activity was performed on (issue, issue_comment, pr, pr_comment, pr_review)
+ *
+ */
 async function getIssueEventType(context) {
   const activityDetail = {
     contributor: "",
@@ -47,6 +59,15 @@ async function getIssueEventType(context) {
   return activityDetail
 }
 
+/**
+ * Retrieves detailed information about an issue event based on the action performed.
+ * @param {*} context
+ * @returns - An object containing the activity detail:
+ * - contributor
+ * - action
+ * - object the activity was performed on (issue, issue_comment, pr, pr_comment, pr_review)
+ *
+ */
 async function getIssueCommentEventType(context) {
   const activityDetail = {
     contributor: context.payload.comment.user.login,
@@ -56,6 +77,15 @@ async function getIssueCommentEventType(context) {
   return activityDetail
 }
 
+/**
+ * Retrieves detailed information about an issue event based on the action performed.
+ * @param {*} context
+ * @returns - An object containing the activity detail:
+ * - contributor
+ * - action
+ * - object the activity was performed on (issue, issue_comment, pr, pr_comment, pr_review)
+ *
+ */
 async function getPullRequestEventType(context) {
   const activityDetail = {
     contributor: context.payload.pull_request.user.login,
@@ -65,6 +95,15 @@ async function getPullRequestEventType(context) {
   return activityDetail
 }
 
+/**
+ * Retrieves detailed information about an issue event based on the action performed.
+ * @param {*} context
+ * @returns - An object containing the activity detail:
+ * - contributor
+ * - action
+ * - object the activity was performed on (issue, issue_comment, pr, pr_comment, pr_review)
+ *
+ */
 async function getPullRequestReviewEventType(context) {
   // also achievable by the context.sender - user who did this event
   const activityDetail = {
@@ -75,6 +114,15 @@ async function getPullRequestReviewEventType(context) {
   return activityDetail
 }
 
+/**
+ * Retrieves detailed information about an issue event based on the action performed.
+ * @param {*} context
+ * @returns - An object containing the activity detail:
+ * - contributor
+ * - action
+ * - object the activity was performed on (issue, issue_comment, pr, pr_comment, pr_review)
+ *
+ */
 async function getPullRequestReviewCommentEventType(context) {
   // also achievable by the context.sender - user who did this event
   const activityDetail = {
